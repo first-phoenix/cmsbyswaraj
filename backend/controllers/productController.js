7de01af60db5e16be01f8b95f9c9750a7f9889c6
first-phoenix <iswarajkhan@gmail.com>
2022-01-03 23:05:48 +0530

    After update product, just remove it, a checkpoint

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,7 +2,7 @@ const Product = require('../models/productModel');
 
 
 
-// create product
+// create product -- Admin
 
 exports.createProduct = async (req, res, next) => {
 
@@ -16,10 +16,36 @@ exports.createProduct = async (req, res, next) => {
 
 
 
+//Gert All Products
+exports.getAllProducts = async(req, res) => {
 
-exports.getAllProducts = (req, res) => {
+    const products = await Product.find();
 
     res.status(200).json({
-        message: 'route is working fine'
+        success: true,
+        products
+    });
+}
+
+//update product -- Admin
+
+exports.updateProduct = async (req, res, next) => {
+    let product = Product.findById(req.params.id);
+
+    if (!product) {
+        return res.status(500).json({
+            success: false,
+            message: 'product not found'
+        });
+    }
+
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
+    });
+
+    res.status(200).json({
+        success: true,
+        product
     });
 }
