46f346d3f925085756988b9fbaac3c8d472bfd13
first-phoenix <iswarajkhan@gmail.com>
2022-01-03 23:30:18 +0530

    added delete product - working fine 1-02 hr

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -27,25 +27,82 @@ exports.getAllProducts = async(req, res) => {
     });
 }
 
-//update product -- Admin
+
+// Update Product -- Admin
 
 exports.updateProduct = async (req, res, next) => {
-    let product = Product.findById(req.params.id);
-
+    let product = await Product.findById(req.params.id);
+  
     if (!product) {
-        return res.status(500).json({
+      return next(new ErrorHander("Product not found", 404));
+    }
+  
+    // Images Start Here
+    // let images = [];
+  
+    // if (typeof req.body.images === "string") {
+    //   images.push(req.body.images);
+    // } else {
+    //   images = req.body.images;
+    // }
+  
+    // if (images !== undefined) {
+    //   // Deleting Images From Cloudinary
+    //   for (let i = 0; i < product.images.length; i++) {
+    //     await cloudinary.v2.uploader.destroy(product.images[i].public_id);
+    //   }
+  
+    //   const imagesLinks = [];
+  
+    //   for (let i = 0; i < images.length; i++) {
+    //     const result = await cloudinary.v2.uploader.upload(images[i], {
+    //       folder: "products",
+    //     });
+  
+    //     imagesLinks.push({
+    //       public_id: result.public_id,
+    //       url: result.secure_url,
+    //     });
+    //   }
+  
+    //   req.body.images = imagesLinks;
+    // }
+  
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+      useFindAndModify: false,
+    });
+  
+    res.status(200).json({
+      success: true,
+      product,
+    });
+  };
+
+
+// Delete Product
+
+exports.deleteProduct = async (req, res, next) => {
+    const product = await Product.findById(req.params.id);
+  
+    if (!product) {
+    //   return next(new ErrorHander("Product not found", 404));
+        return res.status(404).json({
             success: false,
-            message: 'product not found'
+            message: 'Product not found'
         });
     }
-
-    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-        runValidators: true
-    });
-
+  
+    // Deleting Images From Cloudinary
+    // for (let i = 0; i < product.images.length; i++) {
+    //   await cloudinary.v2.uploader.destroy(product.images[i].public_id);
+    // }
+  
+    await product.remove();
+  
     res.status(200).json({
-        success: true,
-        product
+      success: true,
+      message: "Product Delete Successfully",
     });
-}
+  };
